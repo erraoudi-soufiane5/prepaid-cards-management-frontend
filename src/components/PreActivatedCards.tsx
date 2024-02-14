775f1247031687fccc2f57fbdfbc1182b00902fd
erraoudi-soufiane <erraoudisoufiane5@gmail.com>
2024-02-14 19:13:09 +0100

    create cards binding without security

--- src/components/PreActivatedCards.tsx
+++ src/components/PreActivatedCards.tsx
@@ -6,16 +6,62 @@ import {
   Th,
   Stack,
   Button,
+  useToast,
 } from "@chakra-ui/react";
-import React from "react";
+import React, { useState } from "react";
 import PreActivatedCard from "./PreActivatedCard";
 import { IPreActivatedCard } from "../Model/PreActivatedCard";
+import axios from "axios";
 
 interface Props {
   preActivatedCards: IPreActivatedCard[];
 }
 
 const PreActivatedCards = ({ preActivatedCards }: Props) => {
+  const toast = useToast()
+
+  const handleActivation = async () => {
+
+    const requestData = preActivatedCards.map((card) => ({
+      identite: card.cinPassport,
+      nom: card.beneficiaryName,
+      ddn: card.birthDate,
+      tele: card.phone,
+      sex: card.sex.toUpperCase(),
+      entreprise: card.company,
+    }));
+
+    console.log("requestData: ",requestData)
+    
+    axios
+    .post("http://localhost:8000/api/card/creat", requestData)
+    .then((response) => {
+      const { data } = response;
+      if (response.status === 200) {
+        console.log("operation succeded")
+        toast({
+          title: 'Account created.',
+          description: "We've created the requested accounts for you.",
+          status: 'success',
+          duration: 9000,
+          isClosable: true,
+        })
+        preActivatedCards = []
+
+      } else {
+        console.error("Oops, something went wrong.");
+      }
+    })
+    .catch((error) => {
+      console.error(
+        "post request failed",
+        error.response ? error.response.data : error
+      );
+    });
+
+    console.log("keeep going")
+    console.log("I m here : ", preActivatedCards)
+  }
   return (
     <>
       <TableContainer mb={"9px"} borderRadius={14} bg="#ffffff">
--- src/routing/Dashboard.tsx
+++ src/routing/Dashboard.tsx
@@ -1,4 +1,4 @@
-import { Box, Grid, GridItem } from "@chakra-ui/react";
+import { Box, Flex, Grid, GridItem, Heading } from "@chakra-ui/react";
 
 function Dashboard() {
   return (
@@ -10,13 +10,26 @@ function Dashboard() {
         gap={4}
         padding={3}
       >
-        <GridItem
+         <GridItem
           minHeight={"190px"}
           borderRadius={14}
           colSpan={2}
           bg="#ffffff"
+          padding={4} 
         >
-          balance
+          
+          <Heading as="h2" size="md" mb={8} fontWeight={"medium"} textAlign="left">
+            Balance
+          </Heading>
+          
+          <Flex
+            justify="center"
+            height="100%"
+            fontSize="2xl"
+            color="teal.500"
+          >
+            $1,234.56 USD
+          </Flex>
         </GridItem>
         <GridItem borderRadius={14} colSpan={3} rowSpan={2} bg="#ffffff">
           insights
